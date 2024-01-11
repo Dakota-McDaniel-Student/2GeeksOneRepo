e19cc7194482ce427e0b0721a6a97bb2c5e0bea0
Dakota McDaniel <144153283+Dakota-McDaniel-Student@users.noreply.github.com>
2024-01-11 19:44:25 +0000

    Start

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Hello
+﻿public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("What game would you like to play");
+        string Hangman = "HANGMAN";
+        if(Console.ReadLine() == "Hangman")
+        {
+            Console.WriteLine("You are now playing Hangman.");
+            
+        }
+    }
+    
+    
+}
---
+++ games/hangman.cs
@@ -0,0 +1,8 @@
+// public class Hangman
+// {
+
+//     Random rng = new Random();
+//    int 
+//
+//
+// }
