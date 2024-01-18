443d4477bb6726ccc280c8d33e6eeadbeb67d7a4
Dakota McDaniel <144153283+Dakota-McDaniel-Student@users.noreply.github.com>
2024-01-18 17:13:14 +0000

    Update

--- games/hangman.cs
+++ games/hangman.cs
@@ -1,17 +1,25 @@
-public class Hangman
+public static class Hangman
 {
+
+// If need to test the random in the array
+// Console.Write(words[start]);
+
+    public static void Game()
+    {
+
+        Random();
+        Console.Write(words[start]);
+        //Need code to display underscores and maybe hangman character, has to have if statements for when player guesses a correct letter
+
+    }
+}
+
+public static string Random()
+{
+
     //Chooses random word
     static string[] words = { "Excellent", "Good", "Super", "Funny", "Cheeseburger", "Computer" };
     Random random = new Random();
     int start = random.Next(0, words.Length);
 
-// If need to test the random in the array
-// Console.Write(words[start]);
-
-    public void Game()
-    {
-
-        //Need code to display underscores and maybe hangman character, has to have if statements for when player guesses a correct letter
-
-    }
 }
